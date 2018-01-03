9b1f25c632875a1eb1a0efa300ac05d483dbd331
iolufemi <iolufemi@ymail.com>
2018-01-03 22:34:40 +0100

    fixed merge conflict and updated deployment key

--- config/development.js
+++ config/development.js
@@ -18,7 +18,7 @@ module.exports = {
     letsencryptSSLVerificationBody: process.env.LETSENCRYPT_VERIFICATION_BODY || 'xvArhQBSilF4V30dGUagNAZ96ASipB0b0ex0kXn0za8._v6aFbaRYWeOmSebtlD-X4Ixf5tPsyULMsXM8HjsK-Q',
     maxContentLength: process.env.MAX_CONTENT_LENGTH || '9999',
     enforceSSL: process.env.ENFORCE_SSL || 'no',
-    gitOAuthToken: process.env.GIT_OAUTH_TOKEN || '1f4b52a48226258e3ae040323155b064035350cc',
+    gitOAuthToken: process.env.GIT_OAUTH_TOKEN || '86d6eb7abe03e8ae6a970cb67622e667c9c0f86a',
     queueUIUsername: process.env.QUEUE_UI_USERNAME || 'admin',
     queueUIPassword: process.env.QUEUE_UI_PASSWORD || 'password123/',
     queueUIPort: process.env.QUEUE_UI_PORT || 3000,
