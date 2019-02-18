786da2439a3428764dff6e96474eb64abe3014a8
Olufemi Olanipekun <iolufemi@ymail.com>
2019-02-18 23:01:47 +0100

    handles sql associations

--- models/index.js
+++ models/index.js
@@ -1,13 +1,29 @@
 "use strict";
+var _ = require('lodash');
 
 var models = {};
 var normalizedPath = require("path").join(__dirname, "./");
 
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-    var splitFileName = file.split('.');
-    if(splitFileName[0] !== 'index'){
-        models[splitFileName[0]] = require('./'+splitFileName[0]);
-    }
+var files = require("fs").readdirSync(normalizedPath);
+var filesCount = files.length * 1;
+var count = 0;
+var associate = function(models){
+	_.forOwn(models, function(value, key){
+		if(value.associate){
+			value.associate(models);
+		}
+	});
+};
+
+files.forEach(function(file) {
+	count = count + 1;
+	var splitFileName = file.split('.');
+	if(splitFileName[0] !== 'index'){
+		models[splitFileName[0]] = require('./'+splitFileName[0]);
+	}
+	if(count === filesCount){
+		associate(models);
+	}
 });
 
 module.exports = models;
